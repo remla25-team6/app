3c5059a1daf5ede492f305acda0437059f00c082
Maarten van der Weide <m.v.vanderweide@student.tudelft.nl>
2025-05-03 12:39:38 +0200

    Added basic mocked ModelService tests

--- src/main/java/com/remla6/app/dto/PredictResponse.java
+++ src/main/java/com/remla6/app/dto/PredictResponse.java
@@ -1,10 +1,12 @@
 package com.remla6.app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PredictResponse {
     private String sentiment;
 }
